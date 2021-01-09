9a01ed8b80c49b8d8e723014e1df2128869fd7a0
Lakshmi-Manjunatha <lakshmijm12@gmail.com>
2021-01-09 23:18:19 +0000

    added customer service

---
+++ customer-service/src/test/java/com/amazon/clone/customers/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.clone.customers.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
